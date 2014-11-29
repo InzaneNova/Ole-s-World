1d667ec0a1d7eef0ccbc6aa585f83e4fb6fcbd51
Ole M. S <strohm99@gmail.com>
2014-11-29 12:28:53 +0100

    Added testing render function for online players

---
+++ src/no/Strohm/game2D/world/Multiplayer/OnlinePlayers.java
@@ -0,0 +1,38 @@
+package no.Strohm.game2D.world.Multiplayer;
+
+import no.Strohm.game2D.graphics.Screen;
+import no.Strohm.game2D.graphics.SpriteSheet;
+
+public class OnlinePlayers {
+
+	public static boolean serverOn = false;
+
+	playerManagement playerManager;
+
+	public OnlinePlayers() {
+		playerManager = new playerManagement();
+		playerManager.start();
+	}
+
+	public void render(Screen screen, int xPos, int yPos) { // WIP
+		int xx = xPos - 8;
+		int yy = yPos - 8;
+		screen.render(SpriteSheet.mobs, 0, 0, 8, xx, yy, true, 0);
+		screen.render(SpriteSheet.mobs, 1, 0, 8, xx + 8, yy, true, 0);
+		screen.render(SpriteSheet.mobs, 0, 1, 8, xx, yy + 8, true, 0);
+		screen.render(SpriteSheet.mobs, 1, 1, 8, xx + 8, yy + 8, true, 0);
+	}
+
+}
+
+class playerManagement extends Thread {
+
+	public playerManagement() {
+
+	}
+
+	public void run() {
+
+	}
+
+}
